6341e64822979bf442ec5d89b72a62890c5a0a19
AlejandroBran <00224824@uca.edu.sv>
2024-06-12 10:44:50 -0600

    gdb

---
+++ C++ejercicios.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x1;
+    int x2;
+    int resultado;
+    cout << "\nIngrese el primer numero: ";
+    cin >> x1;
+    cout << "\nIngrese el segundo numero: ";
+    cin >> x2;
+    resultado = x1 * x2;
+
+    cout << "\nEl resultado es: "<< resultado << "\n";
+
+
+
+
+    return 0;
+}
